a8d6045d7b40daf4aab502672f99bb67f03b252e
Alejandro Gomez <agomper@gmail.com>
2016-02-23 23:02:08 +0100

    Read UART information sent by Piksi using Serial

---
+++ Piksi/Piksi.ino
@@ -0,0 +1,47 @@
+#include <QueueList.h>
+QueueList <byte> PackageQueue;
+
+void setup() {
+  Serial.begin(115200);                                
+  Serial1.begin (115200);
+  Serial.println("USB Ready!");
+}
+
+void loop() {
+  while (Serial1.available () > 0) {
+    processByte (Serial1.read ());           
+  }
+}
+
+void processByte (const byte inByte) {
+   static int PosCounter = 0, PackageLength = 10;
+   static bool NewPackage = false;
+   
+   if (inByte == 0x55) {
+      //Serial.print("New Package!");
+      PosCounter = 0;
+      NewPackage = true;
+      PackageQueue.push(inByte);
+      //PosCounter++;
+   }
+   else if (NewPackage == true) {
+      PackageQueue.push(inByte);
+      PosCounter++;
+      if (PosCounter == 5) {
+        PackageLength = inByte + 8;
+        //Serial.print(PackageLength);
+      }
+   }
+
+   if (PosCounter == PackageLength - 1) {
+      while (PackageQueue.isEmpty() == false) {
+        Serial.print(PackageQueue.peek(), HEX);
+        Serial.print(" ");
+        PackageQueue.pop();
+      }
+      Serial.println();
+      PosCounter = 0;
+      NewPackage = false;
+   }
+}
+
