2fa6a81ff2820d1d08117916e90cc23b8c3fda18
Alejandro Gomez <agomper@gmail.com>
2016-02-23 23:48:22 +0100

    Show specific messages

--- Piksi/Piksi.ino
+++ Piksi/Piksi.ino
@@ -9,39 +9,63 @@ void setup() {
 
 void loop() {
   while (Serial1.available () > 0) {
-    processByte (Serial1.read ());           
+    process_byte (Serial1.read ());           
   }
 }
 
-void processByte (const byte inByte) {
-   static int PosCounter = 0, PackageLength = 10;
-   static bool NewPackage = false;
+void process_byte (const byte inByte) {
+   static int PackageLength = 10;
+   static bool NewPackage, MessageCode1, MessageCode2 = false;
    
    if (inByte == 0x55) {
       //Serial.print("New Package!");
-      PosCounter = 0;
       NewPackage = true;
       PackageQueue.push(inByte);
-      //PosCounter++;
    }
    else if (NewPackage == true) {
-      PackageQueue.push(inByte);
-      PosCounter++;
-      if (PosCounter == 5) {
-        PackageLength = inByte + 8;
-        //Serial.print(PackageLength);
+      if (PackageQueue.count() == 1 && inByte == 2) {
+        //Serial.print("MessageCode1 True");
+        //Serial.println();
+        MessageCode1 = true;
+        PackageQueue.push(inByte);
+      }
+      else if (PackageQueue.count() == 2 && inByte == 2 && MessageCode1 == true) {
+        //Serial.print("MessageCode2 True");
+        //Serial.println();
+        MessageCode2 = true;
+        PackageQueue.push(inByte);
+      }
+      else if (MessageCode2 == true) {
+        if (PackageQueue.count() == 5)
+          PackageLength = inByte + 8;
+        PackageQueue.push(inByte);
+      }
+      else if (MessageCode2 == false){
+        clean_queue();
+        NewPackage = false;
       }
    }
 
-   if (PosCounter == PackageLength - 1) {
-      while (PackageQueue.isEmpty() == false) {
+   if (PackageQueue.count() == PackageLength - 1) {
+      show_queue();
+      NewPackage = false;
+      MessageCode1 = MessageCode2 = false;
+   }
+}
+
+void show_queue() {
+    while (PackageQueue.isEmpty() == false) {
         Serial.print(PackageQueue.peek(), HEX);
         Serial.print(" ");
         PackageQueue.pop();
-      }
-      Serial.println();
-      PosCounter = 0;
-      NewPackage = false;
-   }
+    }
+    Serial.println();
+}
+
+
+void clean_queue() {
+    while (PackageQueue.isEmpty() == false) {
+        PackageQueue.pop();
+    }
 }
 
